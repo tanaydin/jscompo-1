f3527b02dfc1f1c50811e9b3ddfe07075ffe4524
Tanaydin Sirin <t.sirin@youweagency.com>
2021-07-05 17:54:06 +0200

    s fonksiyonunu tamamen f fonksiyonu içine taşıdım. 143b

--- compo.js
+++ compo.js
@@ -1,7 +1,3 @@
 function f(p, a, b) {
-    return r = b.x - a.x, u = b.y - a.y, t = max(0, min(1, ((p.x - a.x) * r + (p.y - a.y) * u) / s(r, u))), s(p.x - (a.x + t * r), p.y - (a.y + t * u)) ** .5
-}
-
-function s(a, b) {
-    return sq(a) + sq(b)
+    return r = b.x - a.x, u = b.y - a.y, g = max(0, min(1, ((p.x - a.x) * r + (p.y - a.y) * u) / (sq(r) + sq(u)))), (sq(p.x - (a.x + g * r)) + sq(p.y - (a.y + g * u))) ** .5
 }
