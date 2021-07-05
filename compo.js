604459b49641fff7504589309f9013f3861952c8
Tanaydin Sirin <t.sirin@youweagency.com>
2021-07-05 18:04:04 +0200

    Ortak hesaplama yapılan iki noktayı degişkene taşıdım. 131b

--- compo.js
+++ compo.js
@@ -1,3 +1,3 @@
 function f(p, a, b) {
-    return r = b.x - a.x, u = b.y - a.y, g = max(0, min(1, ((p.x - a.x) * r + (p.y - a.y) * u) / (sq(r) + sq(u)))), (sq(p.x - a.x - g * r) + sq(p.y - a.y - g * u)) ** .5
+    return r = b.x - a.x, u = b.y - a.y, o = p.y - a.y, k = p.x - a.x, t = max(0, min(1, (k * r + o * u) / (sq(r) + sq(u)))), (sq(k - t * r) + sq(o - t * u)) ** .5
 }
