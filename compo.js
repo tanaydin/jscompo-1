4d822411f874e07063f93a53c5c1248eb8db0098
Tanaydin Sirin <t.sirin@youweagency.com>
2021-07-05 17:20:13 +0200

    Öklid uzunluğunu hesaplayan fonksiyonun işlevini, direkt çağırıldığı noktaya taşıdım. 210b

--- compo.js
+++ compo.js
@@ -1,12 +1,8 @@
 function f(p,a,b) {
     var t = ((p.x - a.x) * (b.x - a.x) + (p.y - a.y) * (b.y - a.y)) / s(a, b);
     t = Math.max(0, Math.min(1, t));
-    return e(p, { x: a.x + t * (b.x - a.x),
-        y: a.y + t * (b.y - a.y) });
-}
-
-function e(a, b) {
-    return sqrt(s(a, b))  ;
+    return sqrt(s(p, { x: a.x + t * (b.x - a.x),
+        y: a.y + t * (b.y - a.y) }));
 }
 
 function s(a, b) {
