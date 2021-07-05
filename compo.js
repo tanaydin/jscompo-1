42417f00bb9d76cbbe1f83e4e342dfcbc831f84d
Tanaydin Sirin <t.sirin@youweagency.com>
2021-07-05 17:46:59 +0200

    Karakök almak için sqrt() yerine **.5 kullandım. 172b

--- compo.js
+++ compo.js
@@ -1,8 +1,8 @@
 function f(p, a, b) {
-    return r = b.x - a.x, u = b.y - a.y, t = max(0, min(1,((p.x - a.x) * r + (p.y - a.y) * u) / s(a, b))), sqrt(s(p, {
+    return r = b.x - a.x, u = b.y - a.y, t = max(0, min(1, ((p.x - a.x) * r + (p.y - a.y) * u) / s(a, b))), (s(p, {
         x: a.x + t * r,
         y: a.y + t * u
-    }))
+    })) ** .5
 }
 
 function s(a, b) {
