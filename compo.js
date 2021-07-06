621a5081778c3aace23529956aa94f4dd3dc3cac
Tanaydin Sirin <t.sirin@youweagency.com>
2021-07-06 18:19:34 +0200

    Normal fonksiyon tanimlama yerine, oklu ifade (arrow expresssion) kullandim. 116b

--- compo.js
+++ compo.js
@@ -1,3 +1,1 @@
-function f(p, a, b) {
-    return r = b.x - a.x, u = b.y - a.y, o = p.y - a.y, k = p.x - a.x, t = max(0, min(1, (k * r + o * u) / (r ** 2 + u ** 2))), (sq(k - t * r) + sq(o - t * u)) ** .5
-}
+f=(p,a,b)=>(t=p.x-a.x,n=b.x-a.x,y=p.y-a.y,d=b.y-a.y,g=max(0,min(1,(t*n+y*d)/(n**2+d**2))),(sq(g*n-t)+sq(g*d-y))**.5)
