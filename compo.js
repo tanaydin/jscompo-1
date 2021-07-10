fe71c43f7d6a1ed63c606c79dba99e08331961af
Tanaydin Sirin <t.sirin@youweagency.com>
2021-07-10 16:24:25 +0200

    Sonuclarin aciklanmasindan sonra diger kodlara baktigimda kare almayi daha kisa yapabilecegimi fark ettim :) . 114b

--- compo.js
+++ compo.js
@@ -1,1 +1,1 @@
-f=(p,a,b)=>(t=p.x-a.x,n=b.x-a.x,y=p.y-a.y,d=b.y-a.y,g=max(0,min(1,(t*n+y*d)/(n**2+d**2))),(sq(g*n-t)+sq(g*d-y))**.5)
+f=(p,a,b)=>(t=p.x-a.x,n=b.x-a.x,y=p.y-a.y,d=b.y-a.y,g=max(0,min(1,(t*n+y*d)/(n*n+d*d))),(sq(g*n-t)+sq(g*d-y))**.5)
